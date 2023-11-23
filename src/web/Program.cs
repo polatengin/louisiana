edc5c25524ae6befc28d3afc46b9327a0bfc4142
polatengin <enpolat@microsoft.com>
2023-11-23 17:15:41 +0000

    adding azure app config capability

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+});
+
+builder.Services.AddAzureAppConfiguration();
+
 var app = builder.Build();
+
+app.UseAzureAppConfiguration();
+
 app.MapGet("/", (IConfiguration config) => config.GetValue<string>("Lousiana:Settings:Message"));
 
 app.Run();
