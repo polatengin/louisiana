72910200a74e0a45ab5254c0529cb09a6f780120
polatengin <enpolat@microsoft.com>
2023-11-23 17:04:08 +0000

    adding configuration capability to the console app

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -5,4 +5,7 @@ using IHost host = Host.CreateApplicationBuilder(args).Build();
 
 Console.WriteLine("Hello, World!");
 
+var config = new ConfigurationBuilder()
+    .Build();
+
 await host.RunAsync();
