382e48e57264843d6b3b1e4c85ace262976a6578
polatengin <enpolat@microsoft.com>
2023-11-23 17:03:36 +0000

    adding hosting capability to the console

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -1,1 +1,8 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateApplicationBuilder(args).Build();
+
 Console.WriteLine("Hello, World!");
+
+await host.RunAsync();
