a105d5c42596688bc3d964da12b2ff5b552b80ea
polatengin <enpolat@microsoft.com>
2023-11-23 16:59:44 +0000

    creating application config resource

--- .iac/deploy.sh
+++ .iac/deploy.sh
@@ -2,3 +2,5 @@ PROJECT_NAME=${1:-$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)}
 LOCATION=${2:-westus}
 
 az group create --name "rg-${PROJECT_NAME}" --location "${LOCATION}"
+
+az appconfig create --resource-group "rg-${PROJECT_NAME}" --name "${PROJECT_NAME}-config" --location "${LOCATION}" --sku "free"
