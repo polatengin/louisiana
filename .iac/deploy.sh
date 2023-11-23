3605ff11185dc20e10c26dfccef8d3eacc347a0b
polatengin <enpolat@microsoft.com>
2023-11-23 17:00:02 +0000

    adding bunch of keys to the config resource

--- .iac/deploy.sh
+++ .iac/deploy.sh
@@ -4,3 +4,9 @@ LOCATION=${2:-westus}
 az group create --name "rg-${PROJECT_NAME}" --location "${LOCATION}"
 
 az appconfig create --resource-group "rg-${PROJECT_NAME}" --name "${PROJECT_NAME}-config" --location "${LOCATION}" --sku "free"
+
+az appconfig kv set --name "${PROJECT_NAME}-config" --key Lousiana:Settings:BackgroundColor --value White -y
+az appconfig kv set --name "${PROJECT_NAME}-config" --key Lousiana:Settings:FontColor --value Black -y
+az appconfig kv set --name "${PROJECT_NAME}-config" --key Lousiana:Settings:FontSize --value 40 -y
+az appconfig kv set --name "${PROJECT_NAME}-config" --key Lousiana:Settings:Message --value "Data from Azure App Configuration" -y
+az appconfig kv set --name "${PROJECT_NAME}-config" --key Lousiana:Settings:Version --value 1 -y
