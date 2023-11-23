9452bff1d178a41ad94d66ade16acf240b9b3075
polatengin <enpolat@microsoft.com>
2023-11-23 17:04:59 +0000

    reading configuration from json file

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -6,6 +6,7 @@ using IHost host = Host.CreateApplicationBuilder(args).Build();
 Console.WriteLine("Hello, World!");
 
 var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true)
     .Build();
 
 await host.RunAsync();
