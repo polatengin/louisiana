f0c5147cbe6d5d1c34e4bed093a57fb80709bca4
polatengin <enpolat@microsoft.com>
2023-11-23 17:05:09 +0000

    reading configuration from environment variables

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -7,6 +7,7 @@ Console.WriteLine("Hello, World!");
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: true)
+    .AddEnvironmentVariables()
     .Build();
 
 await host.RunAsync();
